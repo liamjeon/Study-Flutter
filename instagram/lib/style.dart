8d78a0c70d77e760a881368ba9b89a1518e37aee
liamjeon <89363456+liamjeon@users.noreply.github.com>
2022-01-20 23:02:57 +0900

    [add] bottom-bar style 생성

---
+++ instagram/lib/style.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+var _var1;
+
+var theme = ThemeData(
+  // textButtonTheme: TextButtonThemeData(
+  //   style: TextButton.styleFrom(
+  //     backgroundColor: Colors.grey,
+  //   )
+  // ),
+  bottomNavigationBarTheme: BottomNavigationBarThemeData(
+    backgroundColor: Colors.white,
+    elevation: 2,
+    selectedItemColor: Colors.black,
+  ),
+  iconTheme: IconThemeData(color: Colors.blue),
+  appBarTheme: AppBarTheme(
+    color: Colors.white,
+    elevation: 1,
+    titleTextStyle: TextStyle(color: Colors.black, fontSize: 25),
+    actionsIconTheme: IconThemeData(color: Colors.black),
+  ),
+  textTheme: TextTheme(
+    headline1: TextStyle(color: Colors.black) ,
+    bodyText2: TextStyle(color: Colors.pinkAccent),
+  )
+);
