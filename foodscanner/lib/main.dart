9fd789dc672466b95cef09efe4404d3a6e47aa40
liamjeon <89363456+liamjeon@users.noreply.github.com>
2022-01-25 19:36:23 +0900

    init

--- foodscanner/lib/main.dart
+++ foodscanner/lib/main.dart
@@ -1,8 +1,8 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+import 'package:xml/xml.dart';
 
 void main() => runApp(MyApp());
 
