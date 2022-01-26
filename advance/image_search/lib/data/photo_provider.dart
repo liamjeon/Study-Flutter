8e3bb6752ee9b4999c097e8eccf6d3857decdd25
liamjeon <89363456+liamjeon@users.noreply.github.com>
2022-01-26 17:04:27 +0900

    [update] Streambuilder

--- advance/image_search/lib/data/photo_provider.dart
+++ advance/image_search/lib/data/photo_provider.dart
@@ -1,11 +1,17 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:image_search/data/api.dart';
+import 'package:image_search/model/photo.dart';
 
 //InheritedWidget 문서에있는 코드임
 class PhotoProvider extends InheritedWidget {
   final PixabayApi api;
 
-  const PhotoProvider({
+  final _photoStreamContoller = StreamController<List<Photo>>()..add([]); //스트림을 관리하는 컨트롤러
+  Stream<List<Photo>> get photoStream => _photoStreamContoller.stream;
+
+  PhotoProvider({
     Key? key,
     required this.api,
     required Widget child,
@@ -16,6 +22,11 @@ class PhotoProvider extends InheritedWidget {
         context.dependOnInheritedWidgetOfExactType<PhotoProvider>();
     assert(result != null, 'No PixabayApi found in context');
     return result!;
+  }
+
+  Future<void> fetch(String query) async {
+    final result = await api.fetch(query);
+    _photoStreamContoller.add(result);
   }
 
   @override
--- advance/image_search/lib/ui/home_screen.dart
+++ advance/image_search/lib/ui/home_screen.dart
@@ -17,8 +17,6 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   final _controller = TextEditingController();
 
-  List<Photo> _photos = [];
-
   @override
   void dispose() {
     // TODO: implement dispose
@@ -52,31 +50,39 @@ class _HomeScreenState extends State<HomeScreen> {
                 ),
                 suffixIcon: IconButton(
                     onPressed: () async {
-                      final photos = await photoProvider.api.fetch(_controller.text);
-                      setState(() {
-                        _photos = photos;
-                      });
+                      photoProvider.fetch(_controller.text);
                     },
                     icon: const Icon(Icons.search)),
               ),
             ),
           ),
-          Expanded(
-            child: GridView.builder(
-              padding: const EdgeInsets.all(16.0),
-              itemCount: _photos.length,
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                crossAxisSpacing: 16,
-                mainAxisSpacing: 16,
-              ),
-              itemBuilder: (context, index) {
-                final photo = _photos[index];
-                return PhotoWidget(
-                  photo: photo,
-                );
-              },
-            ),
+          StreamBuilder<List<Photo>>(
+            stream: photoProvider.photoStream,
+            builder: (context, snapshot) {
+              //snapshot을 통해 데이터가 드렁옴
+              if(!snapshot.hasData){
+                return const CircularProgressIndicator();
+              }
+              final photos = snapshot.data!;
+
+              return Expanded(
+                child: GridView.builder(
+                  padding: const EdgeInsets.all(16.0),
+                  itemCount: photos.length,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    crossAxisSpacing: 16,
+                    mainAxisSpacing: 16,
+                  ),
+                  itemBuilder: (context, index) {
+                    final photo = photos[index];
+                    return PhotoWidget(
+                      photo: photo,
+                    );
+                  },
+                ),
+              );
+            }
           ),
         ],
       ),
